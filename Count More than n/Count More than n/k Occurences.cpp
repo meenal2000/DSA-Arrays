9ef4a214db97f14459c11990d34b4d9418767036
meenal2000 <53430411+meenal2000@users.noreply.github.com>
2021-05-14 21:18:22 +0530

    Update k Occurences.cpp

--- Count More than n/Count More than n/k Occurences.cpp
+++ Count More than n/Count More than n/k Occurences.cpp
@@ -1,9 +1,23 @@
 // https://practice.geeksforgeeks.org/problems/count-element-occurences/1#
 // O(n2) - Keep counting freq through a nested for loop and add it to ans if it's eligible
 
-// O(nlogn)  - Sort the array and then, count the freq , add it to ans if it's eligible
+// O(nlogn)  - Sort the array and then, count the freq , add it to ans if it's eligible 
+// space - O(1)
+// cnt = 1;
+// for( i = 1; i<n; ++i )
+// {
+//     if(arr[i] == arr[i-1] )
+//     {
+//             ++cnt;
+//     }
+//         else
+//         {
+//                 update max;
+//                 cnt = 1;
+//         }
+// }        
 
-// O(n) space , O(n) time
+// O(n) space , O(n) time 
 int countOccurence(int arr[], int n, int k) {
         map<int,int> mp;
         int ans = 0;
